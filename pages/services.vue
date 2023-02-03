7c0fc63d9e624ddc4d3cfdee7d67914c30a174f6
kartik patel <kartik@7span.com>
2023-02-03 15:43:31 +0530

    my first demo

--- pages/services.vue
+++ pages/services.vue
@@ -3,4 +3,5 @@
          <h1>Services</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quos dolores necessitatibus libero laudantium, impedit architecto quas quidem quia laboriosam placeat, hic quam velit ipsum amet maiores aliquid cum at.</p>
     </div>
+    n
    </template>
