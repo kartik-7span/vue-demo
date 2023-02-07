da4482b4f70c26d87acc1ec7029dee839450a742
kartik patel <kartik@7span.com>
2023-02-07 19:31:51 +0530

    Vue Demo COde

--- components/the-testimonial/TheTestimonail.vue
+++ components/the-testimonial/TheTestimonail.vue
@@ -1,12 +1,17 @@
 <template>
     <section class="testimonial">
         <div class="testimonial__wrapper">
-            <div class="grid grid-cols-3 lg:grid-cols-3 gap-4">
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
                 <div class="testimonial--content">
                     <h2>{{ title }}</h2>
-                    <span class="pagingInfo"></span>
+                    <div class="counter">
+                        <span id="slide-active">01</span> /
+                        <span id="slide-total"> </span>
+                    </div>
+                    <div class="news__arrows"></div>
                 </div>
                 <div class="testimonial--data col-span-2">
+
                     <div class="testimonial__slider--wrapper">
                         <div class="testimonail-slide">
                             <div class="testimonial--info__wrap">
@@ -15,11 +20,11 @@
                                 </div>
                                 <div class="testimonial--info">
                                     <h5>Aldo P.</h5>
+                                    <img src="../../assets/images/rattings.svg" alt="Rattings">
                                 </div>
-                                <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
-                                    officia consequat duis enim velit
-                                    mollit.Exercitation veniam consequat sunt</p>
                             </div>
+                            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
+                                officia consequat duis enim velit mollit.Exercitation veniam consequat sunt</p>
                         </div>
                         <div class="testimonail-slide">
                             <div class="testimonial--info__wrap">
@@ -28,11 +33,13 @@
                                 </div>
                                 <div class="testimonial--info">
                                     <h5>Aldo P.</h5>
+                                    <img src="../../assets/images/rattings.svg" alt="Rattings">
                                 </div>
-                                <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
-                                    officia consequat duis enim velit
-                                    mollit.Exercitation veniam consequat sunt</p>
                             </div>
+                            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+                                consequat duis enim velit
+                                mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt
+                                ullamco est sit aliqua dolor do</p>
                         </div>
                         <div class="testimonail-slide">
                             <div class="testimonial--info__wrap">
@@ -41,11 +48,12 @@
                                 </div>
                                 <div class="testimonial--info">
                                     <h5>Aldo P.</h5>
+                                    <img src="../../assets/images/rattings.svg" alt="Rattings">
                                 </div>
-                                <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
-                                    officia consequat duis enim velit
-                                    mollit.Exercitation veniam consequat sunt</p>
                             </div>
+                            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+                                consequat duis enim velit mollit.Exercitation veniam consequat sunt nostrud amet. Amet
+                                minim mollit non deserunt ullamcoest sit aliqua dolor do</p>
                         </div>
                         <div class="testimonail-slide">
                             <div class="testimonial--info__wrap">
@@ -54,11 +62,11 @@
                                 </div>
                                 <div class="testimonial--info">
                                     <h5>Aldo P.</h5>
+                                    <img src="../../assets/images/rattings.svg" alt="Rattings">
                                 </div>
-                                <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
-                                    officia consequat duis enim velit
-                                    mollit.Exercitation veniam consequat sunt</p>
                             </div>
+                            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
+                                officia consequat duis enim velit mollit.Exercitation veniam consequat sunt</p>
                         </div>
                         <div class="testimonail-slide">
                             <div class="testimonial--info__wrap">
@@ -67,11 +75,63 @@
                                 </div>
                                 <div class="testimonial--info">
                                     <h5>Aldo P.</h5>
+                                    <img src="../../assets/images/rattings.svg" alt="Rattings">
                                 </div>
-                                <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
-                                    officia consequat duis enim velit
-                                    mollit.Exercitation veniam consequat sunt</p>
                             </div>
+                            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
+                                officia consequat duis enim velit mollit.Exercitation veniam consequat sunt</p>
+                        </div>
+                        <div class="testimonail-slide">
+                            <div class="testimonial--info__wrap">
+                                <div class="testimonial--img">
+                                    <img src="../../assets/images/profile.png" alt="">
+                                </div>
+                                <div class="testimonial--info">
+                                    <h5>Aldo P.</h5>
+                                    <img src="../../assets/images/rattings.svg" alt="Rattings">
+                                </div>
+                            </div>
+                            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
+                                officia consequat duis enim velit mollit.Exercitation veniam consequat sunt</p>
+                        </div>
+                        <div class="testimonail-slide">
+                            <div class="testimonial--info__wrap">
+                                <div class="testimonial--img">
+                                    <img src="../../assets/images/profile.png" alt="">
+                                </div>
+                                <div class="testimonial--info">
+                                    <h5>Aldo P.</h5>
+                                    <img src="../../assets/images/rattings.svg" alt="Rattings">
+                                </div>
+                            </div>
+                            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
+                                officia consequat duis enim velit mollit.Exercitation veniam consequat sunt</p>
+                        </div>
+                        <div class="testimonail-slide">
+                            <div class="testimonial--info__wrap">
+                                <div class="testimonial--img">
+                                    <img src="../../assets/images/profile.png" alt="">
+                                </div>
+                                <div class="testimonial--info">
+                                    <h5>Aldo P.</h5>
+                                    <img src="../../assets/images/rattings.svg" alt="Rattings">
+                                </div>
+                            </div>
+                            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
+                                officia consequat duis enim velit mollit.Exercitation veniam consequat sunt</p>
+                        </div>
+                        <div class="testimonail-slide">
+                            <div class="testimonial--info__wrap">
+                                <div class="testimonial--img">
+                                    <img src="../../assets/images/profile.png" alt="">
+                                </div>
+                                <div class="testimonial--info">
+                                    <h5>Aldo P.</h5>
+                                    <img src="../../assets/images/rattings.svg" alt="Rattings">
+                                </div>
+                            </div>
+                            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
+                                officia consequat duis enim velit mollit.Exercitation veniam consequat sunt</p>
                         </div>
                     </div>
                 </div>
@@ -86,23 +146,123 @@ export default {
         description: String
     },
     mounted() {
-        var $status = $('.pagingInfo');
-        var $slickElement = $('.testimonial__slider--wrapper');
+        var helpers = {
+            addZeros: function (n) {
+                return (n < 10) ? '0' + n : '' + n;
+            }
+        };
 
-        $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-            //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
-            var i = (currentSlide ? currentSlide : 0) + 1;
-            $status.text(i + '/' + slick.slideCount);
-        });
+        function sliderInit() {
+            var $slider = $('.testimonial__slider--wrapper');
+            $slider.each(function () {
+                var $sliderParent = $(this).parent();
+                $(this).slick({
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    dots: false,
+                    prevArrow: false,
+                    nextArrow: false,
+                    infinite: true,
+                    autoplay: true,
+                    autoplaySpeed: 2000,
+                    speed: 600,
+                    centerMode: false,
+                    appendArrows: $('.news__arrows'),
+                    prevArrow: '<div class="news__arrow news__arrow_dir_left"></div>',
+                    nextArrow: '<div class="news__arrow news__arrow_dir_right"></div>',
+                    responsive: [
+                        {
+                            breakpoint: 1200,
+                            settings: {
+                                slidesToScroll: 1,
+                                slidesToShow: 2,
+                            },
+                        },
+                        {
+                            breakpoint: 600,
+                            settings: {
+                                slidesToScroll: 1,
+                                slidesToShow: 1,
+                            },
+                        },
+                        {
+                            breakpoint: 480,
+                            settings: {
+                                slidesToScroll: 1,
+                                slidesToShow: 1,
+                            },
+                        },
+                    ],
+                });
 
-        $slickElement.slick({
-            autoplay: true,
-            dots: false,
-            slidesToScroll: 1,
-            slidesToShow: 3,
-        });
+                if ($(this).find('.testimonail-slide').length > 1) {
+                    $(this).siblings('.slides-numbers').show();
+                }
+
+                $(this).on('afterChange', function (event, slick, currentSlide) {
+                    $sliderParent.find('.slides-numbers .active').html(helpers.addZeros(currentSlide + 1));
+                    $('.counter > #slide-active').html(helpers.addZeros(currentSlide + 1));
+                });
+
+                var sliderItemsNum = $(this).find('.slick-slide').not('.slick-cloned').length;
+                $sliderParent.find('.slides-numbers .total').html(helpers.addZeros(sliderItemsNum));
+                $('.counter > #slide-total').html(helpers.addZeros(sliderItemsNum));
+
+            });
+        };
+
+        sliderInit();
     }
 
 
 }
 </script>
+<style>
+.testimonial__slider--wrapper .slick-slide {
+    opacity: .4;
+}
+
+.counter {
+    @apply text-gray font-bold;
+}
+
+#slide-active {
+    @apply text-4xl;
+}
+
+#slide-total {
+    @apply text-base text-gray;
+}
+
+.testimonial__slider--wrapper .slick-current {
+    opacity: 1;
+}
+
+.news__arrows {
+    display: flex;
+}
+
+.news__arrow {
+    width: 40px;
+    height: 40px;
+    cursor: pointer;
+    display: block;
+    border: 1px solid white;
+    padding: 10px;
+    background-size: 20px !important;
+    background-position: center center !important;
+    border-radius: 50px;
+    margin-top: 30px;
+}
+
+.news__arrow_dir_left {
+    background: url(../../assets/images/arrow.svg) no-repeat;
+    -webkit-transform: scaleX(-1);
+    transform: scaleX(-1);
+}
+
+.news__arrow_dir_right {
+    background: url(../../assets/images/arrow.svg) no-repeat;
+    margin-left: 10px;
+}
+</style>
